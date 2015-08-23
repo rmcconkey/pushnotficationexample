713a0d8ad9a55267212a58ca1fb22976fa6ce6f8
Rob McConkey <rmcconkey@careered.com>
2015-08-23 13:12:14 -0500

    - code commit

---
+++ app/src/main/java/com/example/rmcconkey/pushnotificationexample/ApplicationConstants.java
@@ -0,0 +1,14 @@
+package com.example.rmcconkey.pushnotificationexample;
+
+/**
+ * Created by rmcconkey on 8/23/15.
+ */
+public interface ApplicationConstants {
+    // Php Application URL to store Reg ID created
+    static final String APP_SERVER_URL = "http://192.168.2.4:9000/gcm/gcm.php?shareRegId=true";
+
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "837715578074";
+    // Message Key
+    static final String MSG_KEY = "m";
+}
